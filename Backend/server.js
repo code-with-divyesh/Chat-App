2ed8c74423e81f8f6f774d4f65646baaf0be6485
code-with-divyesh <gandhidivyesh2308@hotmail.com>
2025-09-06 17:16:40 +0530

    add route,controller ,database and socket.io in server.js

---
+++ Backend/server.js
@@ -0,0 +1,54 @@
+const dotenv = require("dotenv");
+dotenv.config();
+const express = require("express");
+const { Server, Socket } = require("socket.io");
+const cors = require("cors");
+const connectDB = require("./Config/db");
+const chatRouter = require("./Routes/ChatRoute");
+const ChatMessage = require("./Model/ChatMessage");
+const app = express();
+const server = http.createServer(app); // HTTP server for Socket.io
+const io = new Server(server, {
+  cors: { origin: "*", methods: ["GET", "POST"] }, // frontend URL after deployment
+});
+app.use(cors());
+app.use(express.json());
+
+connectDB();
+
+app.use("/api/chat", chatRouter);
+app.get("/", (req, res) => {
+  res.send("Server running🫡👌");
+});
+
+//socket-io
+io.on("connection", (socket) => {
+  console.log("User connected:", socket.id);
+
+  ChatMessage.find()
+    .then((message) => socket.emit("initialMessages", message))
+    .catch((err) => console.log(err));
+
+  //for listen new message
+  socket.on("sendMessage", async (data) => {
+    try {
+      const newMessage = new ChatMessage({
+        user: data.user,
+        message: data.message,
+      });
+      await newMessage.save();
+
+      // Broadcast to all connected clients
+      io.emit("newMessage", newMessage);
+    } catch (err) {
+      console.error(err);
+    }
+  });
+    socket.on("disconnect", () => console.log("User disconnected:", socket.id));
+});
+
+const PORT = process.env.PORT || 5000;
+
+server.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+});
