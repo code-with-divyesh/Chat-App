fa2f9ddbb16049f6940d10b720c9197084799d08
code-with-divyesh <gandhidivyesh2308@hotmail.com>
2025-09-06 17:17:32 +0530

    wrap chatRoom in app

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import ChatRoom from "./ChatRoom";
+import "./App.css";
+const App = () => {
+  return (
+    <div className="App">
+      <ChatRoom />
+    </div>
+  );
+};
+
+export default App;
